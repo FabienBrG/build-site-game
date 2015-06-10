e05b3029f06f265d6a73b1ebbaeffc5a9085289c
FabienBrG <waofab36@hotmail.fr>
2015-06-10 16:35:36 +0200

    Create views of subForum
    Issues on link from the navbar (forum/forum)
    Histoire du (./) dans le src="" <a>

--- src/Site/ForumBundle/Controller/ForumController.php
+++ src/Site/ForumBundle/Controller/ForumController.php
@@ -9,10 +9,20 @@ class ForumController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        
+
         $forums = $em->getRepository('SiteForumBundle:Forum')->findAll();
-                
-        return $this->render('SiteForumBundle:Forum:forum.html.twig', 
+
+        return $this->render('SiteForumBundle:Forum:forum.html.twig',
                              array('forums' => $forums));
+    }
+
+	public function subForumAction($idSubForum)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $subForums = $em->getRepository('SiteForumBundle:SubForum')->findBy(array('id' => $idSubForum));
+
+        return $this->render('SiteForumBundle:Forum:subForum.html.twig',
+                             array('subForums' => $subForums));
     }
 }
