25312008b0a7d24bd62d50b637bfe33c58c15735
FabienBrG <waofab36@hotmail.fr>
2015-06-16 12:16:50 +0200

    Add form new comment on topic

--- src/Site/ForumBundle/Controller/ForumController.php
+++ src/Site/ForumBundle/Controller/ForumController.php
@@ -3,6 +3,7 @@
 namespace Site\ForumBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ForumController extends Controller
 {
@@ -26,13 +27,41 @@ class ForumController extends Controller
                              array('subForums' => $subForums));
     }
 
-	public function topicAction($idSubForum, $idTopic)
+	public function topicAction(Request $request, $idSubForum, $idTopic)
     {
         $em = $this->getDoctrine()->getManager();
 
         $topic= $em->getRepository('SiteForumBundle:Topic')->findBy(array('id' => $idTopic));
+		$auteur = $em->getRepository('SiteForumBundle:Auteur')->findAll();
+
+		// ->setAction($this->generateUrl('parc_info_modifier', array('idmat' => $materiel->getId())))
+		$form = $this->createFormBuilder()
+			->setMethod('POST')
+			->add('pseudo', 'entity', array('class' => 'SiteForumBundle:Auteur',
+                  'property' => 'pseudo'))
+			->add('comment','textarea')
+			->add('envoyer', 'submit')
+            ->getForm();
+
+		if ($form->handleRequest($request)->isSubmitted()) {
+            if ($form->get('envoyer')->isClicked()) {
+                $data = $form->getData();
+
+				$comment = new \Site\ForumBundle\Entity\Comment();
+
+				$comment->setContenu($data['comment']);
+				$comment->setFkAuteur($data['pseudo']);
+				$comment->setDatePublication(new \DateTime('now'));
+				\Doctrine\Common\Util\Debug::dump($topic);
+				$comment->setFkTopic($topic[0]);
+
+				$em->persist($comment);
+				$em->flush();
+
+			}
+		}
 
         return $this->render('SiteForumBundle:Forum:topic.html.twig',
-                             array('topic' => $topic, 'subForum' => $idSubForum));
+                             array('topic' => $topic, 'subForum' => $idSubForum, 'form' => $form->createView()));
     }
 }
